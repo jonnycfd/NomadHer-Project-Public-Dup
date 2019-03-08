c3696f4541cce7085fa6bdfd3fcdcab52338588d
Lixuan Luo <luolixuan@luolixuandeMacBook-Pro.local>
2019-03-08 10:47:46 -0500

    Video component initialized

--- deliverables/nomadher-frontend/App.js
+++ deliverables/nomadher-frontend/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import Video from 'react-native-video';
+import {Constants, Video} from 'expo'
+import VideoComponent from './Video.js'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+      // <View style={styles.container}>
+      //   <Text style={{textAlign: 'center', fontSize: 25, fontWeight:'bold'}}> Video Verification</Text>
+      <VideoComponent />
+      // </View>
+      
+    )
   }
 }
 
@@ -17,5 +22,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
