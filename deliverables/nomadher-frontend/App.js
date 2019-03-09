68a956d07a879bcb91799fe9b852c88b3f8a255c
PufanZhao <pufanzhao@PufanZhaosMacBook-Pro.local>
2019-03-09 16:10:56 -0500

    Comment out the unused imports. Remove the codes in return. Import Login.js and call it in main function.

--- deliverables/nomadher-frontend/App.js
+++ deliverables/nomadher-frontend/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 // import Video from 'react-native-video';
-import {Constants, Video} from 'expo'
-import VideoComponent from './feature_component/Video.js'
+// import {Constants, Video} from 'expo'
+// import VideoComponent from './feature_component/Video.js'
 import { Container, Content, Header, Form, Input, Item, Button, Label} from 'native-base';
-import * as firebase from 'firebase';
-import { Navigation } from 'react-native-navigation';
-
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyD5lrnpxS_2Kg__rx081B-uaXYLj_lgRG4",
-  authDomain: "nomadherd2.firebaseapp.com",
-  databaseURL: "https://nomadherd2.firebaseio.com",
-  projectId: "nomadherd2",
-  storageBucket: "nomadherd2.appspot.com",
-};
-
-firebase.initializeApp(firebaseConfig);
+// import * as firebase from 'firebase';
+import Login from './feature_component/Login.js'
+// import { Navigation } from 'react-native-navigation';
 
 /*
 export default class App extends React.Component {
@@ -32,56 +22,13 @@ export default class App extends React.Component {
 }
 */
 
-export default class LogIn extends React.Component {
-
-  componentDidMount() {
-
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user != null) {
-        console.log(user)
-      }
-    })
-  }
-
-  async loginWithFacebook() {
-    const {type, token} = await Expo.Facebook.logInWithReadPermissionsAsync
-    ('2214679281946238', {permissions:['public_profile']})
-
-    if (type == 'success'){
-      const credential = firebase.auth.FacebookAuthProvider.credential(token)
-
-      firebase.auth().signInWithCredential(credential).catch((error) =>{
-        console.log(error)
-      })
-    }
-  }
-
+export default class App extends React.Component {
   render() {
     return (
-      <Container style={styles.container}>
-        <Form>
-          <Button style={{mariginTop:10}}
-            full
-            rounded
-            primary
-            onPress={() => this.loginWithFacebook()}
-          >
-              <Text style={{ color: 'white' }}> Login With Facebook</Text>
-          </Button>
-
-        </Form>
-      
-      </Container>
+      <Login />
     );
   }
 }
-
-
-
-
-
-
-
 
 
 const styles = StyleSheet.create({
