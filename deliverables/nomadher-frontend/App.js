0c79fc6f51be8a6f5452e08c2f3a342d5de34dda
tonyuuu <tonycl.wu@mail.utoronto.ca>
2019-03-09 17:10:17 -0500

    add camera component

--- deliverables/nomadher-frontend/App.js
+++ deliverables/nomadher-frontend/App.js
@@ -3,20 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 // import Video from 'react-native-video';
 import {Constants, Video} from 'expo'
 import VideoComponent from './Video.js'
-import { Container, Content, Header, Form, Input, Item, Button, Label} from 'native-base';
-import * as firebase from 'firebase';
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyD5lrnpxS_2Kg__rx081B-uaXYLj_lgRG4",
-  authDomain: "nomadherd2.firebaseapp.com",
-  databaseURL: "https://nomadherd2.firebaseio.com",
-  projectId: "nomadherd2",
-  storageBucket: "nomadherd2.appspot.com",
-};
+import CountDown from './feature_component/countdown.js'
+import TakePhoto from './feature_component/TakePhoto.js'
 
-firebase.initializeApp(firebaseConfig);
-
+function onFinish() {
+  console.log("Finish!")
+}
 
 export default class App extends React.Component {
   render() {
@@ -24,7 +17,8 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Test, this is the login page.</Text>
-        <CountDown initCount={20}/>
+        <CountDown initCount={5} passIn={onFinish}/>
+        <TakePhoto />
       </View>
     )
   }
--- deliverables/nomadher-frontend/feature_component/countdown.js
+++ deliverables/nomadher-frontend/feature_component/countdown.js
@@ -4,13 +4,23 @@ import { StyleSheet, Text, View } from 'react-native'
 export default class CountDown extends Component {
 	constructor(props) {
 		super(props)
-		this.state = { countDown: this.props.initCount? this.props.initCount: 10}
+		this.state = { 
+			countDown: this.props.initCount? this.props.initCount: 10, 
+			finish: false,
+		}
 	}
 
 	componentDidMount() {
+		// Update the countdown value.
 		setInterval (() => {
-			if (this.state.countDown > 0) {
+			if (!this.state.finish) {
 				this.setState({countDown: this.state.countDown - 1})
+				
+				// Stop count down and excute the passin function.
+				if (this.state.countDown === 0) {
+					this.setState({finish: true})
+					this.props.passIn()
+				}
 			}
 		}, 1000)
 	}
