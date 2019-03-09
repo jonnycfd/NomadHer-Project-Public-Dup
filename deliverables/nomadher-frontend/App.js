1b12325c8d9e913581a195a4cfc710b1ca0387f9
tonyuuu <tonycl.wu@mail.utoronto.ca>
2019-03-09 17:49:53 -0500

    fix merge mistake

--- deliverables/nomadher-frontend/App.js
+++ deliverables/nomadher-frontend/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import CountDown from './feature_component/countdown.js'
-import TakePhoto from './feature_component/TakePhoto.js'
 import { StyleSheet, Text, View } from 'react-native';
 // import Video from 'react-native-video';
-// import {Constants, Video} from 'expo'
-// import VideoComponent from './feature_component/Video.js'
-import { Container, Content, Header, Form, Input, Item, Button, Label} from 'native-base';
-// import * as firebase from 'firebase';
-import Login from './feature_component/Login.js'
-// import { Navigation } from 'react-native-navigation';
+import {Constants, Video} from 'expo'
+import VideoComponent from './Video.js'
+
+import CountDown from './feature_component/countdown.js'
+import TakePhoto from './feature_component/TakePhoto.js'
 
 function onFinish() {
   console.log("Finish!")
@@ -17,36 +14,16 @@ function onFinish() {
 export default class App extends React.Component {
   render() {
     return (
-      // <View style={styles.container}>
-        // <Text style={{textAlign: 'center', fontSize: 25, fontWeight:'bold'}}> Video Verification</Text>
-        // <VideoComponent />
-        
-      // </View>
-      // <CountDown />
-      // <Login />
-      // <Hello />
-
-      <AppContainer />
-      
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Test, this is the login page.</Text>
+        <CountDown initCount={5} passIn={onFinish}/>
+        <TakePhoto />
+      </View>
     )
   }
 }
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Login />
-    );
-  }
-}
-
-const AppSwitchNavigator = createSwitchNavigator({
-  Login:{screen: Login},
-  hello:{screen: Hello},
-  countdown:{screen: CountDown}
-});
-
-const AppContainer = createAppContainer(AppSwitchNavigator);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
