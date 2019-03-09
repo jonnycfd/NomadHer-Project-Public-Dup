99d319edd4a3384493ef1fa75285b83aae831e38
kongjia1 <kongjiayan19970124@126.com>
2019-03-08 19:28:07 -0500

    connect to firebase

--- deliverables/nomadher-backend/README.md
+++ deliverables/nomadher-backend/README.md
@@ -1,3 +1,4 @@
 Steps to run the backend:
 1. pip3 install -r requirements.txt
 2. python3 main.py
+3. pip3 install --upgrade firebase-admin
--- deliverables/nomadher-backend/main.py
+++ deliverables/nomadher-backend/main.py
@@ -1,6 +1,27 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import db
+
+cred = credentials.Certificate("nomadherd2-firebase-adminsdk-1l2wh-a12557d542.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# Try to create a set in database
+doc_ref = db.collection(u'users').document(u'user1')
+doc_ref.set({
+    u'fullName': u'Tom Kim'
+})
+
+# Try to get data from database
+users_ref = db.collection(u'users')
+docs = users_ref.get()
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
 
 # Start the app and setup the static directory for the html, css, and js files.
 app = Flask(__name__)
@@ -25,5 +46,4 @@ def addBlocking():
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=os.environ.get('PORT', 80))
-
+    app.run(host='127.0.0.1', debug=True, port=os.environ.get('PORT', 8080))
