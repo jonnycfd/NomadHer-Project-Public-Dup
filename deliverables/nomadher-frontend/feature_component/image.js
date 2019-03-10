62d2430acdf1a1c27f9bb4d085b782188caa5a7b
Lixuan Luo <lixuan.luo@mail.utoronto.ca>
2019-03-09 21:34:41 -0500

    image component added

---
+++ deliverables/nomadher-frontend/feature_component/image.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+// import Video from 'react-native-video';
+import {Constants} from 'expo'
+
+export default class SampleImage extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{textAlign: 'center', fontSize: 25, fontWeight:'bold'}}> Sample Image</Text>
+
+        <Image
+          style={{width: "100%", height: "75%"}}
+          source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+        />
+        
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
