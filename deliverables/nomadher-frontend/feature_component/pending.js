17c1d22cd50e1945c79d1d1d630598925038dad2
Lixuan Luo <lixuan.luo@mail.utoronto.ca>
2019-03-10 21:07:53 -0400

    add pending ui

---
+++ deliverables/nomadher-frontend/feature_component/pending.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+// import Video from 'react-native-video';
+import {Constants} from 'expo'
+
+
+
+export default class Pending extends React.Component {
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{textAlign: 'center', fontSize: 25, fontWeight:'bold'}}> Your verification is under review.</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
